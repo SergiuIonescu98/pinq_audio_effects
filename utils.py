756adcf96317c157321c4beb82fc9c65c9f0f61e
isergiuc@yahoo.com <sergiu.ionescu@rohde-schwarz.com>
2022-05-06 13:09:33 +0300

    util commit

--- utils.py
+++ utils.py
@@ -2,7 +2,10 @@ import numpy as np
 from scipy.io import wavfile
 import matplotlib.pyplot as plot
 import simpleaudio as sa
+import struct
 
+
+#### independent de secunde
 def playNote_16b(note, Fs):
 
     # Ensure that highest value is in 16-bit range
@@ -23,3 +26,26 @@ def plotSignal(t, signal, title="Proccesed Signal"):
     plot.xlabel('Time')
     plot.ylabel('Amplitude')
     plot.show()
+
+
+
+
+
+def _struct_format(sample_width, nb_samples):
+    return {1:"%db", 2:"<%dh", 4:"<%dl"}[sample_width] % nb_samples
+
+def read_samples(wave_file):
+    frame_data = wave_file.readframes(wave_file.getnframes())
+    if frame_data:
+        sample_width = wave_file.getsampwidth()
+        nb_samples = len(frame_data) // sample_width
+        format = {1:"%db", 2:"<%dh", 4:"<%dl"}[sample_width] % nb_samples
+        return struct.unpack(format, frame_data)
+    else:
+        return ()
+
+def write_samples(wave_file, samples, sample_width):
+    format = _struct_format(sample_width, len(samples))
+    frame_data = struct.pack(format, *samples)
+    wave_file.writeframes(frame_data)
+    return wave_file
